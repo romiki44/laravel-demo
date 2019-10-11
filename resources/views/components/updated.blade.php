05982b46dc5abbaedef94be35499ab1172d7c019
Roman Kiss <romiki44@hotmail.com>
2019-10-11 18:06:17 +0200

    section14 done

---
+++ resources/views/components/updated.blade.php
@@ -0,0 +1,8 @@
+<p class="text-muted">
+    {{ empty(trim($slot)) ? 'Added' : $slot}} {{$date->diffForHumans()}}
+    @if(isset($name))
+        by {{$name}}
+    @endif
+</p>
+
+
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,18 +1,29 @@
 @extends('layout')
 
 @section('content')
-<h1>{{$post->id}} {{$post->title}}</h1>
+<h1>
+    {{$post->id}} {{$post->title}}
+    @badge(['type'=>'primary', 'show'=>now()->diffInMinutes($post->created_at)<150])
+        Brand new Post!
+    @endbadge
+</h1>
 
-<p>Added:{{$post->created_at->diffForHumans()}}</p>
+<p>{{$post->content}}</p>
+
+@updated(['date'=>$post->created_at, 'name'=>$post->user->name])
+@endupdated
+
+@updated(['date'=>$post->updated_at])
+    Updated
+@endupdated
 
 <h4>Comments</h4>
 @forelse($post->comments as $comment)
     <p>
         {{$comment->content}}
     </p>
-    <p class="text-muted">
-        added {{$comment->created_at->diffForHumans()}}
-    </p>
+    @updated(['date'=>$comment->created_at])
+    @endupdated
 @empty
     <p>No comments yet!</p>
 @endforelse
